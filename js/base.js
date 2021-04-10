48b70ff68be68d773eb8382d881fea8eb126f34d
a-telier <Alexandra.rabi@outlook.com>
2021-04-10 18:12:36 +0000

    added jquery library via cdn

--- js/base.js
+++ js/base.js
@@ -1,27 +1,38 @@
+// SIDENAV
 // document.addEventListener('DOMContentLoaded', function() {
 //     var elems = document.querySelectorAll('.sidenav');
 //     var instances = M.Sidenav.init(elems, options);
-//   });
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-  // Or with jQuery
-
-$(document).ready(function(){
-$('.sidenav').sidenav();
-});
-
-
-// CAROUSEL
-// document.addEventListener('DOMContentLoaded', function() {
-// var elems = document.querySelectorAll('.carousel');
-// var instances = M.Carousel.init(elems, options);
 // });
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 
 // Or with jQuery
 
-$(document).ready(function(){
-$('.carousel').carousel();
+// $(document).ready(function(){
+//     $('.sidenav').sidenav();
+//     $('.dropdown-trigger').dropdown({ hover: false });
+//     $('select').formSelect();
+//     $('.collapsible').collapsible();
+// });
+
+// navigation toggler
+$(document).ready(function() {
+    $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown({ hover: false });
+    $('select').formSelect();
+    $('.collapsible').collapsible();
 });
+
+// CAROUSEL
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
